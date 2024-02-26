9aa2f7435fe1cd2690caa4744d3af693272a22fc
Tadgh Wagstaff <tadgh@tadghwagstaff.com>
2024-02-26 15:48:53 +0000

    remove testing setting

--- backup-virtual-machines.py
+++ backup-virtual-machines.py
@@ -19,7 +19,7 @@ def backup_instances(zone):
             first_disk_name = first_disk_url.split('/')[-1] if first_disk_url else None
             if first_disk_url:
                 last_snapshot_date = get_last_snapshot_date(snapshots_client, first_disk_url)
-                if not last_snapshot_date or (datetime.now(timezone.utc) - last_snapshot_date) > timedelta(days=0):
+                if not last_snapshot_date or (datetime.now(timezone.utc) - last_snapshot_date) > timedelta(days=1):
                     logging.info(f"{instance.name} >> {first_disk_name} had its last snapshot at {last_snapshot_date}.")
                     creation_thread = threading.Thread(target=create_snapshot, args=(snapshots_client, zone, instance, first_disk_name, first_disk_url))
                     creation_thread.start()
